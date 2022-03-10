58d2badff1a0cd0f132ec5c972e2d9cd0585d1d8
Roy Vanegas <roy@thecodeeducators.com>
2022-03-10 05:35:59 -0500

    Update output

--- javascript/gulp/project-scaffold/dev/scripts/app.js
+++ javascript/gulp/project-scaffold/dev/scripts/app.js
@@ -2,8 +2,8 @@ window.onload = () => {
     let body = document.querySelector(`body`);
     let paragraph = document.createElement(`p`);
     let text = document.createTextNode(`If you can see this content in blue with ` +
-        `a light asopdjfopjasdoifjoisdjfthen  ` +
-        `working. Look at the “dev” folder for placing your HTML, Sass, and ` +
+        `a light blue background, then JavaScript and CSS are ` +
+        `working. Look at the “dev” folder for placing your HTML, CSS, and ` +
         `JavaScript.`);
 
     paragraph.appendChild(text);
