dff9851a52f0e170ae9b6d5263d59e1cd370a6b5
Roy Vanegas <roy@thecodeeducators.com>
2022-03-21 14:41:19 -0400

    Add simple example of var vs let

---
+++ javascript/var-vs-let/js/app.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    let message = document.getElementById(`msg`);
+    message.textContent += y;
+    let x = 10; // Rarely NOT use
+    var y = 20; // Rarely use
+    console.log(x);
+    message.textContent += x;
+};
