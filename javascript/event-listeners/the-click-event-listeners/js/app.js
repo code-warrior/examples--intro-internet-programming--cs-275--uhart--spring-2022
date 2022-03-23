a47f15a60c52cbd1e77d3acb640eaabb1b0083d4
Roy Vanegas <roy@thecodeeducators.com>
2022-03-23 15:08:08 -0400

    Add click event listener example

---
+++ javascript/event-listeners/the-click-event-listeners/js/app.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+    let leftButton = document.getElementById(`left-button`),
+        rightButton = document.getElementById(`right-button`),
+        errorStyles = `background-color: red;
+            padding: 2px;
+            color: white;
+            font-family: monospace;
+            border-radius: 4px;`,
+        resetStyles = `background-color: inherit;
+            padding: inherit;
+            font-family: inherit;
+            border-radius: none`;
+
+    function showAlert () {
+        alert(`You clicked the ${this.id}`);
+    }
+
+    if (null !== leftButton) {
+        leftButton.addEventListener(`click`, showAlert);
+    } else {
+        console.error(`A reference to ID %c left-button %c could not be established`, errorStyles, resetStyles);
+    }
+
+    if (null !== rightButton) {
+        rightButton.addEventListener(`click`, showAlert);
+    } else {
+        console.error(`A reference to ID %c right-button %c could not be established`, errorStyles, resetStyles);
+    }
+};
