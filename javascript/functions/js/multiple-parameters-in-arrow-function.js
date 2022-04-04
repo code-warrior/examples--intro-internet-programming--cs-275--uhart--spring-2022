ad34f8804a7d8a401e45747a52a898f9998fffca
Roy Vanegas <roy@thecodeeducators.com>
2022-04-04 15:18:48 -0400

    Add example of an arrow function with two parameters

---
+++ javascript/functions/js/multiple-parameters-in-arrow-function.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    // Traditional function notation of the same function on line 7
+    // function sum( firstParam, secondParam ) {
+    //     return firstParam + secondParam;
+    // }
+
+    let sum = (firstParam, secondParam) => firstParam + secondParam;
+
+    alert(sum(3,4));
+};
