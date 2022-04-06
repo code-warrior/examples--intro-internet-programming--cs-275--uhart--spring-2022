269e9b4ee87b64868903df96fcbdf889ba111e63
Roy A Vanegas <roy@thecodeeducators.com>
2022-04-06 14:57:18 -0400

    An example of returning one item by an arrow function

---
+++ javascript/functions/js/single-return.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    // let getPet = () => {
+    //     return `dog`;
+    // };
+
+    let getPet = () => `dog`;
+
+    // let getPetName = () => {
+    //     return `Aria`;
+    // };
+
+    let getPetName = () => `Aria`;
+
+    alert(`My ${getPet()}’s name is ${getPetName()}`);
+};
